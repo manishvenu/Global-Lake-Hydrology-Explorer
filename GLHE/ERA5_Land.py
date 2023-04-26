f70eb000c2c035ff6732f8c25822d66403548819
manishrv <manishrv@umich.edu>
2023-04-26 01:13:26 -0400

    Initial ERA5 Explanation

--- GLHE/ERA5_Land.py
+++ GLHE/ERA5_Land.py
@@ -1,5 +1,83 @@
 import cdsapi
+import xarray as xr
 
 
-def request_temperature_data():
-    return 3
+def check_if_module_works() -> str:
+    """A simple check if user is able to access functions in this file"""
+
+    return "Success"
+
+
+def get_ERA5_Land_total_precip_runoff_evap_in_sub_region(west: float, east: float, south: float, north: float) -> xr.Dataset:
+    """Gets ERA5 Land precip, evap, & runoff data in a known subregion by lat-long
+
+    Parameters
+    ----------
+    west : str
+        west direction(-180,180), must be less than east
+    east : str
+        east direction(-180,180), must be greater than west
+    south : str
+        south direction(-90,90), must be less than north
+    north : float
+        north direction(-90,90), must be greater than south
+
+    Returns
+    -------
+    xarray Dataset
+        NetCDF format of the evap, precip, & runoff in a grid
+    """
+
+    c = cdsapi.Client()
+    try:
+        response = c.retrieve(
+            'reanalysis-era5-land-monthly-means',
+            {
+                'product_type': 'monthly_averaged_reanalysis',
+                'variable': [
+                    'runoff', 'total_evaporation', 'total_precipitation',
+                ],
+                'year': [
+                    '1950', '1951', '1952',
+                    '1953', '1954', '1955',
+                    '1956', '1957', '1958',
+                    '1959', '1960', '1961',
+                    '1962', '1963', '1964',
+                    '1965', '1966', '1967',
+                    '1968', '1969', '1970',
+                    '1971', '1972', '1973',
+                    '1974', '1975', '1976',
+                    '1977', '1978', '1979',
+                    '1980', '1981', '1982',
+                    '1983', '1984', '1985',
+                    '1986', '1987', '1988',
+                    '1989', '1990', '1991',
+                    '1992', '1993', '1994',
+                    '1995', '1996', '1997',
+                    '1998', '1999', '2000',
+                    '2001', '2002', '2003',
+                    '2004', '2005', '2006',
+                    '2007', '2008', '2009',
+                    '2010', '2011', '2012',
+                    '2013', '2014', '2015',
+                    '2016', '2017', '2018',
+                    '2019', '2020', '2021',
+                    '2022',
+                ],
+                'month': [
+                    '01', '02', '03',
+                    '04', '05', '06',
+                    '07', '08', '09',
+                    '10', '11', '12',
+                ],
+                'time': '00:00',
+                'area': [
+                    north, west, south,
+                    east,
+                ],
+                'format': 'netcdf',
+            }, )
+    except:
+        print("Error calling API")
+        return
+    return xr.open_dataset(response['stream'])
--- tests/early_tests.py
+++ tests/early_tests.py
@@ -1,3 +1,10 @@
-from .context import GLHE
+from GLHE import ERA5_Land
 
 
+
+def main():
+    print(ERA5_Land.request_temperature_data())
+    print("Ran Main")
+
+if __name__ == "__main__":
+    main()
