bdcc30a951e53af795c29cf03a8993ec7cbd76cc
manishrv <manishrv@umich.edu>
2023-04-29 12:37:41 -0400

    Fix CDS api call & create temporary file cleanup

--- GLHE/helpers.py
+++ GLHE/helpers.py
@@ -1,6 +1,10 @@
+import os
+
 import xarray as xr
 import pandas as pd
-def spatially_average_dataset(dataset:xr.Dataset,var:str)->pd.Series:
+
+
+def spatially_average_dataset(dataset: xr.Dataset, var: str) -> pd.Series:
     """Spatially average xarray data to one value over entire grid
 
         Parameters
@@ -14,10 +18,12 @@ def spatially_average_dataset(dataset:xr.Dataset,var:str)->pd.Series:
         pandas Series
             pandas Series with time & variables
         """
-    lat_name='lat'
+    lat_name = 'lat'
     lon_name = 'lon'
-    return dataset.mean(dim=[lat_name,lon_name]).get(var).to_series()
-def group_dataset_by_month(dataset:xr.Dataset)->xr.Dataset:
+    return dataset.mean(dim=[lat_name, lon_name]).get(var).to_series()
+
+
+def group_dataset_by_month(dataset: xr.Dataset) -> xr.Dataset:
     """Groups xarray data to monthly values
 
         Parameters
@@ -31,3 +37,24 @@ def group_dataset_by_month(dataset:xr.Dataset)->xr.Dataset:
         """
 
     return dataset.resample(time='M').sum()
+
+
+def clean_up_temporary_files() -> list:
+    """removes files that start with TEMPORARY
+
+        Parameters
+        ----------
+        None
+        Returns
+        -------
+        list
+            list of filenames of deleted files
+        """
+
+    deleted_files = []
+    for file in os.listdir(".temp"):
+        if file.split("_")[0] == "TEMPORARY":
+            os.remove(".temp/"+file)
+            deleted_files.append(file)
+    os.rmdir(".temp")
+    return deleted_files
