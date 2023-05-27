c7d66fa715f27bf59b841638de084353a78d06a5
manishrv123 <80477243+manishrv123@users.noreply.github.com>
2023-05-27 15:56:24 -0400

    Adding Github Copilot Here

--- tests/early_tests.py
+++ tests/early_tests.py
@@ -1,19 +1,23 @@
-import GLHE.data_access.ERA5_Land as ERA5_Land
+from GLHE.data_access import ERA5_Land
 from GLHE import helpers
 import xarray as xr
 import numpy as np
 import pandas as pd
 import os
-def test_era5_Land():
+
+
+def test_era5_land():
     era5_dataset = ERA5_Land.get_total_precip_runoff_evap_in_sub_region(1.1, 1.2, 0.5, 0.6)
     print(era5_dataset)
 
-def get_sample_data()->xr.Dataset:
+
+def get_sample_data() -> xr.Dataset:
     return xr.open_dataset(
         r'C:\Users\manis\OneDrive - Umich\Documents\Global Lake Hydrology '
         r'Explorer\tests\DummyData\agg_terraclimate_ppt_1958_CurrentYear_GLOBE.nc')
 
-def get_sample_data_2()->xr.Dataset:
+
+def get_sample_data_2() -> xr.Dataset:
     dims = ('time', 'lat', 'lon')
     time = pd.date_range("2021-01-01T00", "2023-12-31T23", freq="H")
     lat = [0, 1]
@@ -26,7 +30,7 @@ def get_sample_data_2()->xr.Dataset:
 def main():
     if not os.path.exists(".temp"):
         os.mkdir(".temp")
-    dataset = ERA5_Land.get_total_precip_runoff_evap_in_sub_region(0.1,0.2,0.1,0.2)
+    dataset = ERA5_Land.get_total_precip_runoff_evap_in_sub_region(0.1, 0.2, 0.1, 0.2)
     helpers.clean_up_temporary_files()
     print("Ran Main")
 
